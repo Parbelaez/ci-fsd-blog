66138704977ec5aa2b7112ebd15fb630ee668f5a
Parbelaez <132989779+Parbelaez@users.noreply.github.com>
2023-08-25 15:16:59 +0200

    Change the allowed hosts in the settings

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ci-fsd-blog.herokuapp.com', 'localhost', '127.0.0.1:8000',
+ALLOWED_HOSTS = ['ci-fsd-blog.herokuapp.com', 'localhost', '127.0.0.1',
                 'https://ci-fsd-blog-f3f701e55fbc.herokuapp.com/',
                 'ci-fsd-blog-f3f701e55fbc.herokuapp.com/']
 
