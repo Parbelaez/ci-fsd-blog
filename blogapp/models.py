e6ad055dcfb880fcee029324286ad96583e52767
Parbelaez <132989779+Parbelaez@users.noreply.github.com>
2023-08-27 13:14:56 +0200

    Create the comment section

--- blogapp/models.py
+++ blogapp/models.py
@@ -28,7 +28,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_comments")
+    author = models.CharField(max_length=200)
     email = models.EmailField()
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -38,4 +38,4 @@ class Comment(models.Model):
         ordering = ['created_on']
     
     def __str__(self):
-        return self.content
+        return f"Comment {self.content} by {self.author}"
--- blogapp/views.py
+++ blogapp/views.py
@@ -21,6 +21,36 @@ class PostDetail(View):
         context = {
             'post': post,
             'comments': comments,
+            'commented': False,
+            'liked': liked,
+            'comment_form': CommentForm(),
+        }
+        template_name = 'post_detail.html'
+        return render(request, template_name, context)
+    
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved_comment=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.user = request.user.username
+            comment_form.instance.email = request.user.email
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment_form.save()
+        else:
+            comment_form = CommentForm()
+
+        context = {
+            'post': post,
+            'comments': comments,
+            'commented': True,
             'liked': liked,
             'comment_form': CommentForm(),
         }
